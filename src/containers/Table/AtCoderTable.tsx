f8c97947d1d0c9f02b61c195bf692b9adb3f9a89
tsushiy <28962399+tsushiy@users.noreply.github.com>
2020-03-26 20:16:32 +0900

    Improved table view

--- src/containers/Table/AtCoderTable.tsx
+++ src/containers/Table/AtCoderTable.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Tab, Nav } from 'react-bootstrap';
 import TableCell from '../../components/Table/TableCell';
 import { Contest, ProblemMap } from "../../types/apiResponse";
-import { Tab, Nav} from "react-bootstrap"
+import styled from "styled-components";
 
 type Props = {
   contests: Contest[]
@@ -15,57 +15,57 @@ const AtCoderRegularTable: React.FC<Props> = props => {
       Math.max(ProblemNoList.length, currentCount), 0);
   const header = ["A", "B", "C", "D", "E", "F", "F2"].slice(0, maxProblemCount);
   return (
-    <Table responsive>
+    <StyledTable className="table-responsive-sm table-bordered table-hover">
       <thead>
         <tr>
           <th>Contest</th>
-          {header.map((e: string) => (
-            <th key={e}>{e}</th>
+          {header.map((e, k) => (
+            <th key={k}>{e}</th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {props.contests.map((contest: Contest, i) => (
+        {props.contests.map((contest, i) => (
           <tr key={i}>
-            <td>{contest.ContestID.toUpperCase()}</td>
+            <th scope="row">{contest.ContestID.toUpperCase()}</th>
             {header.map((_, j) => (
-              <td key={j}>
+              <React.Fragment key={j}>
                 {contest.ProblemNoList[j] !== undefined
-                  ? <TableCell
-                      title={props.problemMap.get(contest.ProblemNoList[j])?.Title}
-                      problemNo={props.problemMap.get(contest.ProblemNoList[j])?.No}
-                    />
+                  ? <td key={j}>
+                      <TableCell
+                        title={props.problemMap.get(contest.ProblemNoList[j])?.Title}
+                        problemNo={props.problemMap.get(contest.ProblemNoList[j])?.No}
+                      />
+                    </td>
                   : null}
-              </td>
+              </React.Fragment>
             ))}
           </tr>
         ))}
       </tbody>
-    </Table>
+    </StyledTable>
   )
 }
 
 const AtCoderOthersTable: React.FC<Props> = props => {
   return (
     <React.Fragment>
-      {props.contests && Object.values(props.contests).map((contest: Contest, k) => (
-        <React.Fragment key={k}>
-          {contest.Title}
-          <Table responsive>
-            <tbody>
-              <tr>
-                {contest.ProblemNoList.map((e: number) => (
-                  <td key={e}>
-                    <TableCell
-                      title={props.problemMap.get(e)?.Title}
-                      problemNo={props.problemMap.get(e)?.No}
-                    />
-                  </td>
-                ))}
-              </tr>
-            </tbody>
-          </Table>
-        </React.Fragment>
+      {props.contests && Object.values(props.contests).map((contest, k) => (
+        <StyledTable key={k} className="table-responsive-sm table-bordered table-hover">
+          <caption>{contest.Title}</caption>
+          <tbody>
+            <tr>
+              {contest.ProblemNoList.map((e, i) => (
+                <td key={i}>
+                  <TableCell
+                    title={props.problemMap.get(e)?.Title}
+                    problemNo={props.problemMap.get(e)?.No}
+                  />
+                </td>
+              ))}
+            </tr>
+          </tbody>
+        </StyledTable>
       ))}
     </React.Fragment>
   )
@@ -84,39 +84,61 @@ const AtCoderTable: React.FC<Props> = props => {
   })
 
   return (
-    <React.Fragment>
-      <Tab.Container id="atcoder-tabs" defaultActiveKey="abc" transition={false}>
-        <Nav variant="pills" className="flex-row">
-          <Nav.Item>
-            <Nav.Link eventKey="abc">ABC</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="arc">ARC</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="agc">AGC</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="other">Others</Nav.Link>
-          </Nav.Item>
-        </Nav>
-        <Tab.Content>
-          <Tab.Pane eventKey="abc">
-            <AtCoderRegularTable contests={abc} problemMap={problemMap} />
-          </Tab.Pane>
-          <Tab.Pane eventKey="arc">
-            <AtCoderRegularTable contests={arc} problemMap={problemMap} />
-          </Tab.Pane>
-          <Tab.Pane eventKey="agc">
-            <AtCoderRegularTable contests={agc} problemMap={problemMap} />
-          </Tab.Pane>
-          <Tab.Pane eventKey="other">
-            <AtCoderOthersTable contests={others} problemMap={problemMap} />
-          </Tab.Pane>
-        </Tab.Content>
-      </Tab.Container>
-    </React.Fragment>
+    <Tab.Container id="atcoder-tabs" defaultActiveKey="abc" transition={false}>
+      <Nav variant="pills" className="flex-row">
+        <Nav.Item>
+          <Nav.Link eventKey="abc">ABC</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="arc">ARC</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="agc">AGC</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="other">Others</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Tab.Content>
+        <Tab.Pane eventKey="abc">
+          <AtCoderRegularTable contests={abc} problemMap={problemMap} />
+        </Tab.Pane>
+        <Tab.Pane eventKey="arc">
+          <AtCoderRegularTable contests={arc} problemMap={problemMap} />
+        </Tab.Pane>
+        <Tab.Pane eventKey="agc">
+          <AtCoderRegularTable contests={agc} problemMap={problemMap} />
+        </Tab.Pane>
+        <Tab.Pane eventKey="other">
+          <AtCoderOthersTable contests={others} problemMap={problemMap} />
+        </Tab.Pane>
+      </Tab.Content>
+    </Tab.Container>
   )
 }
+
+const StyledTable = styled(Table)`
+  &&& {
+    table-layout: fixed;
+    width: 100%;
+    word-wrap: break-word;
+
+    & > caption {
+      caption-side: top;
+      color: #000;
+    }
+
+    @media (max-width: 767px) {
+      table-layout: auto;
+      display: block;
+      & > thead {
+        display: none;
+      }
+      & > tbody, tr, th, td, caption {
+        display: block;
+      }
+    }
+  }
+`
 
 export default AtCoderTable;
--- src/containers/Table/index.tsx
+++ src/containers/Table/index.tsx
@@ -23,24 +23,26 @@ const ContestTable: React.FC<{}> = () => {
   }, [])
 
   return (
-    <Tab.Container id="domain-tabs" defaultActiveKey="atcoder" transition={false}>
-      <Nav variant="pills" className="flex-row">
-        <Nav.Item>
-          <Nav.Link eventKey="atcoder">AtCoder</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="codeforces">Codeforces</Nav.Link>
-        </Nav.Item>
-      </Nav>
-      <Tab.Content>
-        <Tab.Pane eventKey="atcoder">
-          <AtCoderTable contests={contests} problemMap={problemMap} />
-        </Tab.Pane>
-        <Tab.Pane eventKey="codeforces">
-          codeforces
-        </Tab.Pane>
-      </Tab.Content>
-    </Tab.Container>
+    <div className="container">
+      <Tab.Container id="domain-tabs" defaultActiveKey="atcoder" transition={false}>
+        <Nav variant="pills" className="flex-row">
+          <Nav.Item>
+            <Nav.Link eventKey="atcoder">AtCoder</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="codeforces">Codeforces</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Tab.Content>
+          <Tab.Pane eventKey="atcoder">
+            <AtCoderTable contests={contests} problemMap={problemMap} />
+          </Tab.Pane>
+          <Tab.Pane eventKey="codeforces">
+            codeforces
+          </Tab.Pane>
+        </Tab.Content>
+      </Tab.Container>
+    </div>
   );
 }
 
