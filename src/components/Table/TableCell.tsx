7d03c0263202344b1d337bb8ea55ee205edb13f3
tsushiy <28962399+tsushiy@users.noreply.github.com>
2020-03-30 23:00:35 +0900

    Add edit button and problem url to TableCell

--- src/components/Table/TableCell.tsx
+++ src/components/Table/TableCell.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import { AppState } from '../../types/appState';
 
 type Props = {
@@ -10,13 +11,51 @@ type Props = {
 const TableCell: React.FC<Props> = (props) => {
   const { problemNo } = props;
   const { problemMap } = useSelector((state: AppState) => state.problem);
+  const { myNotesMap } = useSelector((state: AppState) => state.note);
 
-  const title = problemMap.get(problemNo)?.Title;
-  const url = `/my/${problemNo}`;
+  const noteExists = myNotesMap.has(problemNo);
+  const problem = problemMap.get(problemNo);
+  const title = problem?.Title;
+  const editUrl = `/my/${problemNo}`;
+
+  let problemUrl = "";
+  if (problem) {
+    switch (problem.Domain) {
+      case "atcoder":
+        problemUrl = `https://atcoder.jp/contests/${problem.ContestID}/tasks/${problem.ProblemID}`;
+        break;
+      case "codeforces":
+        problemUrl = `https://codeforces.com/contest/${problem.ContestID}/problem/${problem.ProblemID}`
+        break;
+      case "yukicoder":
+        problemUrl = `https://yukicoder.me/problems/no/${problem.FrontendID}`
+        break;
+      case "aoj":
+        problemUrl = `https://onlinejudge.u-aizu.ac.jp/problems/${problem.ProblemID}`
+        break;
+      case "leetcode":
+        problemUrl = `https://leetcode.com/problems/${problem.Slug}/`
+        break;
+    }
+  }
 
   return (
-    <Link to={url}>{title}</Link>
+    <Container>
+      <div>
+        <Link to={editUrl} style={{color: noteExists ? "#39c" : "#555", fontSize: "0.9em", fontWeight: "bold"}}>
+          Edit
+        </Link>
+      </div>
+      <a href={problemUrl} target="_blank">
+        {title}
+      </a>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default TableCell;
