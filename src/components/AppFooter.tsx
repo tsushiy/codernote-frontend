28362920c6461f5700dbf583af53f149733043ee
tsushiy <28962399+tsushiy@users.noreply.github.com>
2020-04-06 22:19:37 +0900

    Create AppFooter

---
+++ src/components/AppFooter.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from "styled-components";
+
+const AppFooter: React.FC<{}> = () => {
+  return (
+    <Container>
+      © 2020 tsushiy
+    </Container>
+  )
+}
+
+export const Container = styled.div`
+  display: block;
+  text-align: center;
+  font-size: 0.9em;
+  font-weight: bold;
+  height: 52px;
+  bottom: 0;
+  padding: 14px 16px;
+  background-color: #fff;
+`;
+
+export default AppFooter;
